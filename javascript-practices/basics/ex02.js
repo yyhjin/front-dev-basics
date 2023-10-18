e381ddff8681bfa95f7d530fb209224dfa779911
yyhjin <wendy0301@naver.com>
2023-10-18 10:51:38 +0900

    ===!!

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -1,15 +1,41 @@
 /**
  * 변수와 자료형
  *
- * 기본타입
+ * [기본타입]
  * - undefined
  * - number
  * - string
  * - boolean
  *
- * 객체
+ * [객체]
  * - object
+ *   1. new 생성자함수() 사용해서 생성
+ *   2. 리터럴
+ *      {}, [], //
+ *   3. null
  * - function
+ *   1. new Function() 사용해서 생성
+ *   2. 리터럴
+ *      var f = function() { ... }
+ *   3. function f() { ... }
+ *
+ */
+
+/**
+ * 자바스크립트 객체를 분류하는 또 다른 방법
+ *
+ * 1. 내장 객체
+ *    javascript engine 내부에 미리 내장(정의)되어 있는 객체
+ *    - 생성자 함수: Number, Boolean, String, Date, ...
+ *    - 일반 함수: parseInt, setTimeout, setInterval
+ *
+ * 2. 호스트 객체
+ *    javascript 엔진이 embed 되어 있는 호스트 환경에 있는 객체
+ *    Brower 호스트: screen, location, navigator, document(DOM), XmlHttpRequest
+ *    Node 호스트: http, os, fs, path, process, server, ...
+ *
+ * 3. 사용자 객체
+ *    javascript 엔진이 실행되면서 생성하는 객체
  *
  */
 
---
+++ javascript-practices/basics/ex03.js
@@ -0,0 +1,37 @@
+/**
+ * undefined와 null의 둥치(equals) 비교
+ */
+
+var myVar1 = undefined; // 명시적으로 undefined 대입
+var myVar2; // 암시적으로 undefined 대입
+var myVar3 = null;
+
+console.log(myVar1, myVar2, myVar3);
+console.log(myVar1 == myVar2); // true: 값(의미) 비교
+console.log(myVar1 === myVar3); // false: 타입비교 + 값비교
+
+console.log("==================");
+// == : equality, 값의 동치성, 형변환
+console.log("4" == 4);
+console.log(false == 0);
+console.log("abc" == new String("abc"));
+
+console.log(true + 10); // 11
+console.log("abc" + new String("abc")); // abcabc
+console.log(1 + "11"); // 111
+console.log(parseInt("11") + 1); // 12
+
+/**
+ * ===
+ * 1. 타입의 동일성
+ * 2. 타입이 같은 경우
+ *    1) primitive type: 값의 동일성
+ *    2) object type: 객체의 동일성
+ */
+
+console.log("4" === 4);
+console.log(true === 1);
+console.log("abc" === new String("abc"));
+console.log(2 === 4);
+console.log(new String("hello") === new Number("hello"));
+console.log("hello" === "hello");
